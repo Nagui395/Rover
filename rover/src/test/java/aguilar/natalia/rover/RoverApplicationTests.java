2b8bca1bc5fa545a077f3c5256a98a4e51d8eee3
Nagui395 <natalia.aguilarmartin@gmail.com>
2025-08-10 21:27:45 +0200

    Inicializacion proyecto con Spring Boot

---
+++ rover/src/test/java/aguilar/natalia/rover/RoverApplicationTests.java
@@ -0,0 +1,13 @@
+package aguilar.natalia.rover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
